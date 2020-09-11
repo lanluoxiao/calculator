6ad241d4f1450de5946b700f1e7ae5e9e48d03e5
lanluoxiao <lanluoxiao@users.noreply.github.com>
2020-09-11 09:24:00 +0000

    deploy: bc031874bd5ce32be88c7f45f5f36b17ee853fc5

---
+++ precache-manifest.a4d44931e1bbd82c4d2bc7cf8af3121f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a881f56f383e331f54e1a6124870b2a1",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "26b65481bbe007bdfaf3",
+    "url": "/calculator/static/js/2.fb38ffad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.fb38ffad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c9af79563b40f536d9e",
+    "url": "/calculator/static/js/main.7dea9455.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
